b07272e8ecb7f16e6641c7e0806974bb2cab802d
y1220 <ga.2.y1220@gmail.com>
2020-11-21 12:29:23 +0100

    created actions of Docs

---
+++ app/controllers/docs_controller.rb
@@ -0,0 +1,33 @@
+class DocsController < ApplicationController
+
+    def index
+    end
+
+    def show
+    end
+
+    def new
+    end
+
+    def create #doesn't have view page
+    end
+
+    def edit
+    end
+
+    def uodate #doesn't have view page
+    end
+
+    def destroy
+    end
+
+    private 
+
+        def find_doc
+        end
+
+        def doc_params
+        end
+
+
+end
