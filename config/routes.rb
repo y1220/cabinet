6911454284994dcfe0b34ec6f8d08e3487bf02ea
y1220 <ga.2.y1220@gmail.com>
2020-11-22 14:09:17 +0100

    changed the root depends on the user

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,11 @@ Rails.application.routes.draw do
   devise_for :users
   get 'welcome/index'
 
-  root "welcome#index"
+  authenticated :user do
+    root "docs#index", as: "authenticated_root"
+  end
+
+  root "welcome#index" #NOT FOR LOGGINED
 
   resources :docs
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
